03d6dd542aefb24d6b45ba322bcbc1b22f4221fe
Grey <grey.nagle@gmail.com>
2019-07-25 23:42:19 -0400

    Explore buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+//import React from 'react';
 import './App.css';
+import classname from 'classnames'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Grid />
     </div>
   );
 }
 
+class Grid extends React.Component {
+  state = {press: true}
 
+  
+  render() {
+    return (
+      <button 
+        className={classname({
+          "buttonClass1":this.state.press,
+          "buttonClass2":!this.state.press
+        })}
+        onMouseDown={() => {this.setState(prevstate => ({press: !prevstate.press}))}}
+        onMouseUp={() => {this.setState(prevstate => ({press: !prevstate.press}))}}
+      >
+        Test
+      </button>
+    )
+  }
+}
 
 export default App;
